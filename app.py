e001c27b21e4f47de24974e730318e44a663b1ad
hindhar <hindhaughrob@gmail.com>
2024-08-30 14:12:28 +0100

    Force HTTPS and update for SSL

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-import os
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    if not request.is_secure:
+        url = request.url.replace('http://', 'https://', 1)
+        return redirect(url, code=301)
 
 def get_tube_stops():
     return [
