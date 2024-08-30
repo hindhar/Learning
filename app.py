60254b2cb7c873a672fa162b184d71bf04c9df94
hindhar <hindhaughrob@gmail.com>
2024-08-30 12:27:52 +0100

    Update app.py to use dynamic port binding for Heroku

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
@@ -98,4 +99,5 @@ def api_find_unique_stop():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
